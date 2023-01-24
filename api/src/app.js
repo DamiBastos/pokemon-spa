15fb2be4e76c4c8a154697b1337b681a38096413
DamiBastos <damian.bastos@yahoo.com>
2023-01-24 11:03:21 -0300

    config env

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const {
+ DB_PORT
+} = process.env;
 
 require('./db.js');
 
@@ -15,7 +19,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', DB_PORT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
